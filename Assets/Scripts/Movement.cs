657760b0a0863b0c12b1237d4de6c6539673a0e9
KrystianWasil <wasilkrystian78@gmail.com>
2025-04-28 16:57:32 +0200

    Correct Animation issue
    Fixed bug with annimation already work

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -1,15 +1,116 @@
+// using System.Collections;
+// using System.Collections.Generic;
+// using UnityEngine;
+
+// public class PlayerController : MonoBehaviour
+// {
+//     public float moveSpeed;
+
+//     private bool isMoving;
+//     public LayerMask solidObjectsLayer;
+
+//     public Transform weaponHolder;
+//     private SpriteRenderer spriteRenderer;
+//     private Animator animator;
+
+//     private Vector2 input;
+
+//     private void Awake()
+//     {
+//         spriteRenderer = GetComponent<SpriteRenderer>();
+//         animator = GetComponent<Animator>(); // Pobierz animator
+//     }
+
+//     private void Update()
+//     {
+//         HandleUpdate();
+//     }
+
+//     public static class GameData
+//     {
+//         public static Vector3 PlayerPosition;
+//     }
+
+//     public void HandleUpdate()
+//     {
+//         if (!isMoving)
+//         {
+//             input.x = Input.GetAxisRaw("Horizontal");
+//             input.y = Input.GetAxisRaw("Vertical");
+
+//             // Flipping postaci
+//             if (input.x > 0)
+//                 spriteRenderer.flipX = false;
+//             else if (input.x < 0)
+//                 spriteRenderer.flipX = true;
+
+//             // Ustaw animację
+//             animator.SetBool("isRunning", input != Vector2.zero); // <-- ZMIANA z "isMoving" na "isRunning"
+
+//             if (input != Vector2.zero)
+//             {
+//                 var targetPos = transform.position;
+//                 targetPos.x += input.x;
+//                 targetPos.y += input.y;
+
+//                 if (isWalkable(targetPos))
+//                     StartCoroutine(Move(targetPos));
+//             }
+//             else
+//             {
+//                 animator.SetBool("isRunning", false); // <-- ZMIANA
+//             }
+//         }
+
+//         if (Input.GetKeyDown(KeyCode.Z))
+//         {
+//             Debug.Log("Pozycja gracza: " + transform.position);
+//         }
+//     }
+
+//     IEnumerator Move(Vector3 targetPos)
+//     {
+//         isMoving = true;
+
+//         while ((targetPos - transform.position).sqrMagnitude > Mathf.Epsilon)
+//         {
+//             transform.position = Vector3.MoveTowards(transform.position, targetPos, moveSpeed * Time.deltaTime);
+//             yield return null;
+//         }
+
+//         transform.position = targetPos;
+//         isMoving = false;
+
+//         animator.SetBool("isRunning", false); // <-- ZMIANA
+//         CheckForEncounters();
+//     }
+
+//     private bool isWalkable(Vector3 targetPos)
+//     {
+//         return Physics2D.OverlapCircle(targetPos, 0.3f, solidObjectsLayer) == null;
+//     }
+
+//     private void CheckForEncounters()
+//     {
+//         if (Physics2D.OverlapCircle(transform.position, 0.1f, solidObjectsLayer) != null)
+//         {
+//             if (Random.Range(1, 100) <= 25)
+//             {
+//                 Debug.Log("Zadzialalo");
+//             }
+//         }
+//     }
+// }
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public float moveSpeed;
+    public float moveSpeed = 5f;
 
     private bool isMoving;
     public LayerMask solidObjectsLayer;
 
-    public Transform weaponHolder;
     private SpriteRenderer spriteRenderer;
     private Animator animator;
 
@@ -18,7 +119,7 @@ public class PlayerController : MonoBehaviour
     private void Awake()
     {
         spriteRenderer = GetComponent<SpriteRenderer>();
-        animator = GetComponent<Animator>(); // Pobierz animator
+        animator = GetComponent<Animator>();
     }
 
     private void Update()
@@ -26,27 +127,22 @@ public class PlayerController : MonoBehaviour
         HandleUpdate();
     }
 
-    public static class GameData
-    {
-        public static Vector3 PlayerPosition;
-    }
-
     public void HandleUpdate()
     {
+        input.x = Input.GetAxisRaw("Horizontal");
+        input.y = Input.GetAxisRaw("Vertical");
+
+        // Ustaw animację natychmiast
+        animator.SetBool("isRunning", input != Vector2.zero);
+
+        // Flipping sprite'a
+        if (input.x > 0)
+            spriteRenderer.flipX = false;
+        else if (input.x < 0)
+            spriteRenderer.flipX = true;
+
         if (!isMoving)
         {
-            input.x = Input.GetAxisRaw("Horizontal");
-            input.y = Input.GetAxisRaw("Vertical");
-
-            // Flipping postaci
-            if (input.x > 0)
-                spriteRenderer.flipX = false;
-            else if (input.x < 0)
-                spriteRenderer.flipX = true;
-
-            // Ustaw animację
-            animator.SetBool("isMoving", input != Vector2.zero);
-
             if (input != Vector2.zero)
             {
                 var targetPos = transform.position;
@@ -56,15 +152,6 @@ public class PlayerController : MonoBehaviour
                 if (isWalkable(targetPos))
                     StartCoroutine(Move(targetPos));
             }
-            else
-            {
-                animator.SetBool("isMoving", false);
-            }
-        }
-
-        if (Input.GetKeyDown(KeyCode.Z))
-        {
-            Debug.Log("Pozycja gracza: " + transform.position);
         }
     }
 
@@ -80,24 +167,10 @@ public class PlayerController : MonoBehaviour
 
         transform.position = targetPos;
         isMoving = false;
-
-        animator.SetBool("isMoving", false); // wróć do Idle po ruchu
-        CheckForEncounters();
     }
 
     private bool isWalkable(Vector3 targetPos)
     {
         return Physics2D.OverlapCircle(targetPos, 0.3f, solidObjectsLayer) == null;
-    }
-
-    private void CheckForEncounters()
-    {
-        if (Physics2D.OverlapCircle(transform.position, 0.1f, solidObjectsLayer) != null)
-        {
-            if (Random.Range(1, 100) <= 25)
-            {
-                Debug.Log("Zadzialalo");
-            }
-        }
     }
 }
