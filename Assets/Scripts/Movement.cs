f71d890b1e13e64204724dc568b196054bbe21a9
KrystianWasil <wasilkrystian78@gmail.com>
2025-05-27 15:44:58 +0200

    meleeatack
    SCRUM-127 – Fix weapon holder rotation and implement a system for dealing damage to enemies.

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -1,106 +1,3 @@
-// using System.Collections;
-// using System.Collections.Generic;
-// using UnityEngine;
-
-// public class PlayerController : MonoBehaviour
-// {
-//     public float moveSpeed;
-
-//     private bool isMoving;
-//     public LayerMask solidObjectsLayer;
-
-//     public Transform weaponHolder;
-//     private SpriteRenderer spriteRenderer;
-//     private Animator animator;
-
-//     private Vector2 input;
-
-//     private void Awake()
-//     {
-//         spriteRenderer = GetComponent<SpriteRenderer>();
-//         animator = GetComponent<Animator>(); // Pobierz animator
-//     }
-
-//     private void Update()
-//     {
-//         HandleUpdate();
-//     }
-
-//     public static class GameData
-//     {
-//         public static Vector3 PlayerPosition;
-//     }
-
-//     public void HandleUpdate()
-//     {
-//         if (!isMoving)
-//         {
-//             input.x = Input.GetAxisRaw("Horizontal");
-//             input.y = Input.GetAxisRaw("Vertical");
-
-//             // Flipping postaci
-//             if (input.x > 0)
-//                 spriteRenderer.flipX = false;
-//             else if (input.x < 0)
-//                 spriteRenderer.flipX = true;
-
-//             // Ustaw animację
-//             animator.SetBool("isRunning", input != Vector2.zero); // <-- ZMIANA z "isMoving" na "isRunning"
-
-//             if (input != Vector2.zero)
-//             {
-//                 var targetPos = transform.position;
-//                 targetPos.x += input.x;
-//                 targetPos.y += input.y;
-
-//                 if (isWalkable(targetPos))
-//                     StartCoroutine(Move(targetPos));
-//             }
-//             else
-//             {
-//                 animator.SetBool("isRunning", false); // <-- ZMIANA
-//             }
-//         }
-
-//         if (Input.GetKeyDown(KeyCode.Z))
-//         {
-//             Debug.Log("Pozycja gracza: " + transform.position);
-//         }
-//     }
-
-//     IEnumerator Move(Vector3 targetPos)
-//     {
-//         isMoving = true;
-
-//         while ((targetPos - transform.position).sqrMagnitude > Mathf.Epsilon)
-//         {
-//             transform.position = Vector3.MoveTowards(transform.position, targetPos, moveSpeed * Time.deltaTime);
-//             yield return null;
-//         }
-
-//         transform.position = targetPos;
-//         isMoving = false;
-
-//         animator.SetBool("isRunning", false); // <-- ZMIANA
-//         CheckForEncounters();
-//     }
-
-//     private bool isWalkable(Vector3 targetPos)
-//     {
-//         return Physics2D.OverlapCircle(targetPos, 0.3f, solidObjectsLayer) == null;
-//     }
-
-//     private void CheckForEncounters()
-//     {
-//         if (Physics2D.OverlapCircle(transform.position, 0.1f, solidObjectsLayer) != null)
-//         {
-//             if (Random.Range(1, 100) <= 25)
-//             {
-//                 Debug.Log("Zadzialalo");
-//             }
-//         }
-//     }
-// }
 using System.Collections;
 using UnityEngine;
 
@@ -110,9 +7,8 @@ public class PlayerController : MonoBehaviour
 
     private bool isMoving;
     public LayerMask solidObjectsLayer;
-
     public LayerMask solidObjectsInvisibleLayer;
-    
+
     public Transform weaponHolder;
     private SpriteRenderer spriteRenderer;
     private Animator animator;
@@ -135,26 +31,32 @@ public class PlayerController : MonoBehaviour
         input.x = Input.GetAxisRaw("Horizontal");
         input.y = Input.GetAxisRaw("Vertical");
 
-        // Ustaw animację natychmiast
         animator.SetBool("isRunning", input != Vector2.zero);
 
-        // Flipping sprite'a
         if (input.x > 0)
+        {
             spriteRenderer.flipX = false;
+
+            if (weaponHolder != null)
+                weaponHolder.localRotation = Quaternion.Euler(0f, 0f, -50f); // obrót broni w prawo
+        }
         else if (input.x < 0)
+        {
             spriteRenderer.flipX = true;
 
-        if (!isMoving)
-        {
-            if (input != Vector2.zero)
-            {
-                var targetPos = transform.position;
-                targetPos.x += input.x;
-                targetPos.y += input.y;
+            if (weaponHolder != null)
+                weaponHolder.localRotation = Quaternion.Euler(0f, 0f, 50f); // obrót broni w lewo
+        }
 
-                if (isWalkable(targetPos))
-                    StartCoroutine(Move(targetPos));
-            }
+        // Ruch
+        if (!isMoving && input != Vector2.zero)
+        {
+            Vector3 targetPos = transform.position;
+            targetPos.x += input.x;
+            targetPos.y += input.y;
+
+            if (isWalkable(targetPos))
+                StartCoroutine(Move(targetPos));
         }
     }
 
@@ -185,7 +87,6 @@ public class PlayerController : MonoBehaviour
         }
         return hit.collider == null;
     }
-
 
     private void CheckForEncounters()
     {
